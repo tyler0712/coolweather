003ebe086d9ac753909b507f4b1dc579e817c6d7
Mark <451861975@qq.com>
2016-05-26 18:21:08 +0800

    -a

---
+++ app/src/main/java/com/coolweather/app/model/Country.java
@@ -0,0 +1,44 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by markIron on 2016/5/26.
+ */
+public class Country {
+
+    private int id;
+    private String countryName;
+    private String countryCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
